4f19f198e56d3395a0eeca645764c4125d69e694
Albertus Alvin <albertusalvin23@gmail.com>
2023-02-22 23:55:42 +0100

    Inject objp from outside. Caller needs objp for extrinsics calibration later.

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ import glob
 import util
 
 
-def calibrate_camera(images, img_scale, board_dim, criteria):
-    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-    objp = np.zeros((board_dim[1] * board_dim[0], 3), np.float32)
-    objp[:,:2] = np.mgrid[0:board_dim[0], 0:board_dim[1]].T.reshape(-1,2)
-
+def calibrate_camera(objp, images, img_scale, board_dim, criteria):
     # Arrays to store object points and image points from all the images.    
     objpoints = [] # 3d point in real world space
     imgpoints = [] # 2d points in image plane.
@@ -21,8 +17,8 @@ def calibrate_camera(images, img_scale, board_dim, criteria):
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
         ret, corners = cv.findChessboardCorners(gray, board_dim, None)
-        print('isCornersFound?', ret)
-        # TODO: Do we need to use manual annotation in this phase? The assignment instruction indicates that manual annotation comes in step 2, when calculating camera extrinsics.
+        # TODO: Do we need to use manual annotation in this phase? 
+        # The assignment instruction indicates that manual annotation comes in step 2, when calculating camera extrinsics.
         
         # If found, add object points, image points (after refining them)
         if ret:
@@ -35,11 +31,10 @@ def calibrate_camera(images, img_scale, board_dim, criteria):
             cv.imshow('img', img)
             cv.waitKey(1000)
     
-    ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-    print('MATRIX')
-    print(mtx)
-    return ret, mtx, dist, rvecs, tvecs
+        print(f'Calibrating with {fname}. Autofound corners: {ret}')
     
+    return cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
 
 
 if __name__ == '__main__':
@@ -48,14 +43,17 @@ if __name__ == '__main__':
     img_scale = 80
     board_width, board_height, square_size = util.get_checkerboard_config()
 
-    # Termination criteria
-    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((board_width * board_height, 3), np.float32)
+    objp[:,:2] = np.mgrid[0:board_width, 0:board_height].T.reshape(-1,2)
+
+    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001) # Termination criteria
 
     # Take sample frames from videos
     util.generate_frames_for_intrinsics(num_frames)
     util.generate_frames_for_extrinsics()
 
-    calibrate_camera(glob.glob('data/cam1/frames/*.jpg'), img_scale, (board_width, board_height), criteria)
-    calibrate_camera(glob.glob('data/cam2/frames/*.jpg'), img_scale, (board_width, board_height), criteria)
-    calibrate_camera(glob.glob('data/cam3/frames/*.jpg'), img_scale, (board_width, board_height), criteria)
-    calibrate_camera(glob.glob('data/cam4/frames/*.jpg'), img_scale, (board_width, board_height), criteria)
+    calibrate_camera(objp, glob.glob('data/cam1/frames/*.jpg'), img_scale, (board_width, board_height), criteria)
+    calibrate_camera(objp, glob.glob('data/cam2/frames/*.jpg'), img_scale, (board_width, board_height), criteria)
+    calibrate_camera(objp, glob.glob('data/cam3/frames/*.jpg'), img_scale, (board_width, board_height), criteria)
+    calibrate_camera(objp, glob.glob('data/cam4/frames/*.jpg'), img_scale, (board_width, board_height), criteria)
